4405a74ca15770576535d777fe79d5aee86908b2
Homyakin <tox.home@ya.ru>
2023-12-06 20:08:34 +0300

    [NF] add Dockerfile for db

---
+++ database/docker.sh
@@ -0,0 +1,4 @@
+docker stop telegram-quest-bot
+docker rm telegram-quest-bot
+docker build -f Dockerfile -t telegram-quest-bot-img .
+docker run --name telegram-quest-bot -p 5432:5432 -d telegram-quest-bot-img
