ea76e433cfe8d49d3d50056055538e1fa14c8c45
Artem Overin <a.overin@qiwi.com>
2023-12-07 11:13:35 +0300

    [NF] add choose for stage

--- src/main/java/ru/homyakin/quest/bot/frontend/views/helloworld/UsersView.java
+++ src/main/java/ru/homyakin/quest/bot/frontend/views/helloworld/UsersView.java
@@ -1,5 +1,6 @@
 package ru.homyakin.quest.bot.frontend.views.helloworld;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -26,6 +27,11 @@ public class UsersView extends VerticalLayout {
     private final Select<QuestShort> selectQuestName = new Select<>();
 
     private final Select<Long> selectUserId = new Select<>();
+
+    private final Select<String> selectStage = new Select<>();
+
+    private final Button clearButton = new Button("Очистить");
+
     private final Grid<UserAnswerResult> answerGrid = new Grid<>();
 
     public UsersView(UserDao userDao, QuestDao questDao) {
@@ -39,6 +45,9 @@ public class UsersView extends VerticalLayout {
         selectUserId.setItems(userDao.getAnswers().stream().map(UserAnswerResult::userId).distinct().toList());
         selectUserId.addValueChangeListener(e -> refreshDataByUser(e.getValue()));
 
+        selectStage.setItems(userDao.getAnswers().stream().map(UserAnswerResult::stageName).distinct().toList());
+        selectStage.addValueChangeListener(e -> refreshDataByStage(e.getValue()));
+
 
         answerGrid.addColumn(UserAnswerResult::userId)
                 .setHeader("User id");
@@ -46,25 +55,41 @@ public class UsersView extends VerticalLayout {
         answerGrid.addColumn(UserAnswerResult::stageName).setHeader("Stage name");
         answerGrid.addColumn(UserAnswerResult::answer).setHeader("Answer");
         answerGrid.addColumn(UserAnswerResult::answerTime).setHeader("Answer time");
-        refreshData(null, null);
+        refreshData(null, null, null);
+
+        clearButton.addClickListener(e -> clearSelects());
         add(
-                new HorizontalLayout(selectQuestName, selectUserId),
-                answerGrid
+            new HorizontalLayout(selectUserId, selectQuestName, selectStage, clearButton),
+            answerGrid
         );
     }
 
+    private void clearSelects() {
+        selectQuestName.clear();
+        selectUserId.clear();
+        refreshData(null, null, null);
+    }
+
+    private void refreshDataByStage(String stageName) {
+        refreshData(selectQuestName.getValue(), selectUserId.getValue(), stageName);
+    }
+
     private void refreshDataByQuestName(QuestShort quest) {
-        refreshData(quest, selectUserId.getValue());
+        refreshData(quest, selectUserId.getValue(), selectStage.getValue());
     }
 
     private void refreshDataByUser(Long userId) {
-        refreshData(selectQuestName.getValue(), userId);
+        refreshData(selectQuestName.getValue(), userId, selectStage.getValue());
     }
 
-    private void refreshData(QuestShort quest, Long userId) {
+    private void refreshData(QuestShort quest, Long userId, String stageName) {
         final var answers = userDao.getAnswers()
                 .stream()
-                .filter(it -> (quest == null || quest.name().equals(it.questName())) && (userId == null || userId.equals(it.userId())))
+                .filter(it ->
+                        (quest == null || quest.name().equals(it.questName())) &&
+                                (userId == null || userId.equals(it.userId())) &&
+                                (stageName == null || stageName.equals(it.stageName()))
+                )
                 .sorted(Comparator.comparing(UserAnswerResult::questName).thenComparing(UserAnswerResult::answerTime))
                 .toList();
         answerGrid.setItems(answers);
